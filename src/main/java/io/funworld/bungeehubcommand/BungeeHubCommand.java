985719c5932d84ead514c1edb9a6acc4e732fa2c
xiaoyi1212 <mc18845340923@163.com>
2024-01-24 13:12:17 +0800

    HubPlugin

--- src/main/java/io/funworld/bungeehubcommand/BungeeHubCommand.java
+++ src/main/java/io/funworld/bungeehubcommand/BungeeHubCommand.java
@@ -7,12 +7,26 @@ import net.md_5.bungee.config.YamlConfiguration;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
 
 public final class BungeeHubCommand extends Plugin {
     String server_name;
     @Override
     public void onEnable() {
         try {
+            if (!getDataFolder().exists())
+                getDataFolder().mkdir();
+            File file = new File(getDataFolder(), "config.yml");
+
+
+            if (!file.exists()) {
+                try (InputStream in = getResourceAsStream("config.yml")) {
+                    Files.copy(in, file.toPath());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
             Configuration configuration = ConfigurationProvider.getProvider(YamlConfiguration.class).load(new File(getDataFolder(), "config.yml"));
             server_name = configuration.get("server","main");
             getProxy().getPluginManager().registerCommand(this, new HubCommand(this));
